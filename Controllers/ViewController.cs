9a652606ef3fc30d11506cb732920402b2d3878a
DaronWasTaken <zwierzchowski.kacper1@gmail.com>
2023-09-23 17:27:31 +0200

    Added levelWidget view

---
+++ Controllers/ViewController.cs
@@ -0,0 +1,24 @@
+﻿using bingo_api.Models.Views;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bingo_api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ViewController : ControllerBase
+{
+    [HttpGet("levelWidget/{id:int}")]
+    public IActionResult getUserLevelWidget(int id)
+    {
+        var levelWidgetDto = new LevelWidgetDto
+        {
+            Username = "James",
+            Level = 2,
+            Points = 2300,
+            RequiredPoints = 3000
+        };
+
+        return Ok(levelWidgetDto);
+    }
+        
+}
---
+++ Models/Views/LevelWidgetDto.cs
@@ -0,0 +1,9 @@
+﻿namespace bingo_api.Models.Views;
+
+public class LevelWidgetDto
+{
+    public string Username { get; set; } = null!;
+    public int Level { get; set; }
+    public int Points { get; set; }
+    public int RequiredPoints { get; set; }
+}
